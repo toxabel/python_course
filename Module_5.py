e0e3e53909afd624931514078ae8b71439b6fadf
Anton Nalivaika <toxabel@gmail.com>
2023-03-19 19:59:44 +0100

    Added file for module 5

---
+++ Module_5.py
@@ -0,0 +1,21 @@
+# Create a tool, which will do user generated news feed:
+#
+# 1.User select what data type he wants to add
+#
+# 2.Provide record type required data
+#
+# 3.Record is published on text file in special format
+#
+#
+#
+# You need to implement:
+#
+# 1.News – text and city as input. Date is calculated during publishing.
+#
+# 2.Privat ad – text and expiration date as input. Day left is calculated during publishing.
+#
+# 3.Your unique one with unique publish rules.
+#
+#
+#
+# Each new record should be added to the end of file. Commit file in git for review.
